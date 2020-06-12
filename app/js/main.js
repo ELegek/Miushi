01e7ddf0de710ae979e0f00e99f2fd9a800b8096
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2020-06-12 18:30:25 +0300

    Блок товары

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,14 @@ $('.intro__slider').slick({
     arrows: true,
     dots: true,
 });
+
+$('.gallery__slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+});
   
 });
