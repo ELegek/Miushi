3638b0207efe13edf46b45aebd65142e439b6b12
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2020-06-16 17:42:40 +0300

    Адаптив меню 2.0

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,23 @@
-$(function(){
+$(function () {
+
+    // ================ MENU BURGER ===================
+    document.querySelector('.menu__burger').onclick = function () {
+        this.classList.toggle('active');
+    };
 
 
-$('.intro__slider').slick({
-    arrows: true,
-    dots: true,
-});
+    $('.intro__slider').slick({
+        arrows: true,
+        dots: true,
+    });
 
-$('.gallery__slider').slick({
-    dots: true,
-    arrows: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-});
-  
+    $('.gallery__slider').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+    });
+
 });
