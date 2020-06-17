21757398e39802fd2a9ca32ce1ab90ef6dda3822
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2020-06-17 12:32:30 +0300

    Адаптивный слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -18,6 +18,42 @@ $(function () {
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows: true,
+              }
+            },
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true,
+                arrows: true,
+              }
+            },
+            {
+              breakpoint: 481,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: true,
+                infinite: true,
+                dots: true,
+                arrows: true,
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
 
 });
