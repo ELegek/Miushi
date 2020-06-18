afcd47435a5a077b92356adf64809545c84bcce6
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2020-06-18 12:13:07 +0300

    Адаптив слайдеров

--- app/js/main.js
+++ app/js/main.js
@@ -32,16 +32,6 @@ $(function () {
             {
               breakpoint: 769,
               settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true,
-                arrows: true,
-              }
-            },
-            {
-              breakpoint: 481,
-              settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
                 centerMode: true,
@@ -53,7 +43,6 @@ $(function () {
             // You can unslick at a given breakpoint now by adding:
             // settings: "unslick"
             // instead of a settings object
-          ]
+        ]
     });
-
 });
