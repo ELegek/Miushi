e1fbe1fd9a1909daad2a02b60c494a22c772a8bc
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2020-06-19 14:35:47 +0300

    Accordeon

--- app/js/main.js
+++ app/js/main.js
@@ -45,4 +45,25 @@ $(function () {
             // instead of a settings object
         ]
     });
+
+
+
+
+    $(function(){
+      // hide all content
+      $('.accordeon_content').hide();
+      
+      $('.accordeon_title').click(function(){
+        $(this).parent().toggleClass('active').siblings().removeClass('active');
+        $('.accordeon_content').slideUp();
+        
+        if(!$(this).next().is(":visible")) {
+          $(this).next().slideDown();
+        }
+      });
+    });
+
+
+
+
 });
